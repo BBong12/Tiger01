9b6c205ea04f645e48cb427ff25f8273484901ea
user <user@192.168.219.104>
2021-05-28 22:20:21 +0900

    1

--- Last1000/src/pack/Hello.java
+++ Last1000/src/pack/Hello.java
@@ -4,6 +4,11 @@ public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println(1);
+		System.out.println(2);
+		System.out.println(3);
+		System.out.println(4);
+		System.out.println(5);
+		System.out.println(6);
 
 	}
 
